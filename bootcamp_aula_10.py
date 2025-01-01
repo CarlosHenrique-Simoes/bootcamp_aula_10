176ffbb200cf91da1c6c68aeeca8e3e8975b7f1e
Carlos Henrique Simoes <carlinhos.ryck2@gmail.com>
2025-01-01 13:55:59 -0400

    Funções | Docstrings

--- bootcamp_aula_10.py
+++ bootcamp_aula_10.py
@@ -1,5 +1,7 @@
 def format_name(first_name, last_name):
 
+    """Take a first and last name and format it to return the title case version of the name"""
+
     if first_name == "" or last_name == "":
         return "You didn't provide valid inputs"
 
