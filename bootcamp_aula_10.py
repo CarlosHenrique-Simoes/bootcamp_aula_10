416208b7f1045a8a7eea551b9d2d27fdab705e4b
Carlos Henrique Simoes <carlinhos.ryck2@gmail.com>
2025-01-01 05:38:15 -0400

    Funções | Outputs multiplos returns

--- bootcamp_aula_10.py
+++ bootcamp_aula_10.py
@@ -1,9 +1,11 @@
 def format_name(first_name, last_name):
+
+    if first_name == "" or last_name == "":
+        return "You didn't provide valid inputs"
+
     first_name = first_name.title()
     last_name = last_name.title()
 
-    return f"{first_name} {last_name}"
+    return f"Result: {first_name} {last_name}"
 
-name = format_name("jimi", "hendrix")
-
-print(name)
+print(format_name(input("Enter your first name: "), input("Enter your last name: ")))
